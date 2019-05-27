6b1388caa1f985ad93a00f2a1e7825902dc94377
Sander <sander_krom@hotmail.com>
2019-05-27 21:31:42 +0200

    Update person.php

--- person.php
+++ person.php
@@ -22,17 +22,7 @@
 		}
 
 		function setDateOfBirth($day, $month, $year) {
-			if ($day <= 0) {
-				$dayOfBirth = 0;
-				$monthOfBirth = 0;
-				$yearOfBirth = 0;
-			}
-			elseif ($month < 1 or $month > 12) {
-				$dayOfBirth = 0;
-				$monthOfBirth = 0;
-				$yearOfBirth = 0;
-			}
-			elseif ($year < 1900 or $year > 2100) {
+			if ($day <= 0 or $month < 1 or $month > 12 or $year < 1900 or $year > 2100) {
 				$dayOfBirth = 0;
 				$monthOfBirth = 0;
 				$yearOfBirth = 0;
